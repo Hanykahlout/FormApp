49f7c858b13b357c424a6f5eeb9571cb54af7461
Hany Alkahlout <hanykahlout@gmail.com>
2023-07-21 00:03:39 +0300

    add tags inn form items and handle show image and note field in form items and update database for from items

---
+++ FormApp/Scenes/QCFormVC/Cells/SideBySideTableViewCell/SideBySideTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SideBySideTableViewCell.swift
+//  FormApp
+//
+//  Created by Hany Alkahlout on 13/07/2023.
+//
+
+import UIKit
+
+class SideBySideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
